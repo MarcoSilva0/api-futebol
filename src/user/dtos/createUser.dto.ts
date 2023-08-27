6c86d5d8a9f6480997ff655767ce008620e45752
MarcoSilva0 <marcoantonio02016@outlook.com>
2023-08-27 18:02:19 -0300

    feat: User Controler init

---
+++ src/user/dtos/createUser.dto.ts
@@ -0,0 +1,8 @@
+export interface CreateUserDto {
+  name: string;
+  email: string;
+  password: string;
+  documentIdentifier: string;
+  phone: string;
+  picture: string;
+}
---
+++ src/user/interfaces/user.interface.ts
@@ -0,0 +1,9 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  password: string;
+  documentIdentifier: string;
+  phone: string;
+  picture: string;
+}
